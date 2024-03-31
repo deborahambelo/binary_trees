14a3ebbc22d4b707497d0f90b74f59edd62e51d0
deborahambelo <ketema.deborah@gmail.com>
2024-03-31 03:23:58 -0700

    task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -3,8 +3,10 @@
 /**
  * binary_tree_delete - function that deletes an entire binary tree
  * @tree: a pointer to the root node of the tree to delete
+ *
+ * Description: This function recursively deletes the entire binary tree
+ *              rooted at the given node.
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree == NULL)
